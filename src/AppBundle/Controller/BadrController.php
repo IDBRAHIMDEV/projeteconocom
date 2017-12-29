057a0de490e6dd184e9b1168a2effdffc7bfd4e8
ZAGDOUNI salma <szag1503@e2rsc.local>
2017-12-29 11:48:15 +0000

    salma first

--- src/AppBundle/Controller/BadrController.php
+++ src/AppBundle/Controller/BadrController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 // this is the official Badr's Controller
-
+// this is edited by Salma 
 class BadrController extends Controller
 {
 }
